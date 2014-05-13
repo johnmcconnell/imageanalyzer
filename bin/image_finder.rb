65f728da12bfb3e4942ab8ac83d6413e5bd563de
johnny <johnnyillinois@gmail.com>
2014-05-12 20:56:04 -0500

    image finder works and passes unit tests

--- bin/image_finder.rb
+++ bin/image_finder.rb
@@ -1,16 +1,39 @@
 require 'google-search'
+require 'open-uri'
 
 class ImageFinder
-  def findImages(query_string,count)
-    Google::Search:Image.new(:query => query_string).filter {|image|
-      i.image_type == :jpg or i.image_type == :png
-    }.map { 
-      |image| image.uri
-    }
+  @@base_folder = File.join("images","raw")
+  
+  def findImages(query_string, count)
+    images = Array.new
+    Google::Search::Image.new(:query => query_string).each do |i|
+      if (images.size < count)
+	images << i
+      end
+    end
+    images
   end
 
   def saveImages(urls)
-    0
+    urls.each_with_index do |image,idx|
+      basename = filenameForUrl(image.uri,idx)
+      filename = File.join(@@base_folder,basename)
+      downloadImageToFile(filename,image.uri)
+    end 
   end
 
+  def downloadImageToFile(filename,url)
+      open(filename,"wb") do |file|
+        file << open(url).read
+      end
+  end
+
+  def filenameForUrl(url,idx)
+    filename = "image-" + idx.to_s + urlToExtension(url)
+  end
+
+  def urlToExtension(url)
+    extensions = url.split(".")
+    last = "." + extensions[-1]
+  end
 end
--- bin/image_spec.rb
+++ bin/image_spec.rb
@@ -1,9 +1,53 @@
-require 'image_finder'
+require_relative 'image_finder'
 
 describe ImageFinder, "grab 10 urls" do
-  it "returns a list of urls" do
+  let(:urls) { [ 
+    	"http://www.sopimc.com/images/popular-credit-cards.jpg",
+	"http://www.sopimc.com/images/popular-credit-cards.jpg",
+	"http://www.sopimc.com/images/popular-credit-cards.jpg",
+	"http://www.sopimc.com/images/popular-credit-cards.jpg",
+	"http://www.sopimc.com/images/popular-credit-cards.jpg",
+	"http://www.sopimc.com/images/popular-credit-cards.jpg",
+	"http://www.sopimc.com/images/popular-credit-cards.jpg",
+	"http://www.sopimc.com/images/popular-credit-cards.jpg",
+	"http://www.sopimc.com/images/popular-credit-cards.jpg",
+	"http://www.sopimc.com/images/popular-credit-cards.jpg",
+	] }
+  let(:extensions) { [
+  	".jpg",".jpg",".jpg",".jpg",".jpg",
+  	".jpg",".jpg",".jpg",".jpg",".jpg"
+	] }
+  let(:filenames) { [
+    	"image-0.jpg","image-1.jpg","image-2.jpg","image-3.jpg","image-4.jpg",
+    	"image-5.jpg","image-6.jpg","image-7.png","image-8.jpg","image-9.jpg",
+	] }
+  
+  it "converts the image url to file extension" do
+    finder = ImageFinder.new
+    
+    matches = extensions.zip(urls)
+    
+    matches.each do |ext,url|
+      expect(ext).to eq(finder.urlToExtension(url))
+    end
+  end
+
+  it "returns a filename for an image url" do
+    finder = ImageFinder.new
+    matches = filenames.zip(urls)
+    
+    matches.each_with_index do |item,idx|
+      expect(item[0]).to eq(finder.filenameForUrl(item[1],idx))
+    end
+  end
+
+  it "returns a list of images and saves them to disk" do
     count = 10
-    results = @image_finder.findImages('credit cards',count)
-    results.size.should eq(count)
+    finder = ImageFinder.new
+
+    results = finder.findImages('credit cards',count)
+    expect(results.size).to eq(count)
+    
+    finder.saveImages(results)
   end
 end
